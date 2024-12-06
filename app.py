f7c1bdead66b211eb88bfb194233c2144ab06561
joviyal-arun <joviyalarun07@gmail.com>
2024-12-06 16:04:23 +0530

    branch changes

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
 # Modules 
 import os
 
-import cv2
-
-def read_image(image_path):
-  """Reads an image from the specified path.
-
-  Args:
-    image_path: The path to the image file.
-
-  Returns:
-    The image as a NumPy array.
-  """
-
-  img = cv2.imread(image_path)
-  if img is None:
-    print("Error: Could not read the image.")
-    return None
-  return img
